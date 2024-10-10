649756acb40057512c77aa4f0b723d3e65717d74
Gesang Widigdo <gesangwidigdo@gmail.com>
2024-10-11 01:37:31 +0700

    fix: using g4g algorithm

--- des_algorithm.py
+++ des_algorithm.py
@@ -1,210 +1,367 @@
-# Tabel Initial Permutation (IP)
-IP = [
-    58, 50, 42, 34, 26, 18, 10, 2,
-    60, 52, 44, 36, 28, 20, 12, 4,
-    62, 54, 46, 38, 30, 22, 14, 6,
-    64, 56, 48, 40, 32, 24, 16, 8,
-    57, 49, 41, 33, 25, 17, 9, 1,
-    59, 51, 43, 35, 27, 19, 11, 3,
-    61, 53, 45, 37, 29, 21, 13, 5,
-    63, 55, 47, 39, 31, 23, 15, 7
-]
+# Tables
 
-# Tabel Final Permutation (FP)
-FP = [
-    40, 8, 48, 16, 56, 24, 64, 32,
-    39, 7, 47, 15, 55, 23, 63, 31,
-    38, 6, 46, 14, 54, 22, 62, 30,
-    37, 5, 45, 13, 53, 21, 61, 29,
-    36, 4, 44, 12, 52, 20, 60, 28,
-    35, 3, 43, 11, 51, 19, 59, 27,
-    34, 2, 42, 10, 50, 18, 58, 26,
-    33, 1, 41, 9, 49, 17, 57, 25
-]
+# Table of Position of 64 bits at initial level: Initial Permutation Table
+initial_perm = [
+      58, 50, 42, 34, 26, 18, 10, 2,
+      60, 52, 44, 36, 28, 20, 12, 4,
+      62, 54, 46, 38, 30, 22, 14, 6,
+      64, 56, 48, 40, 32, 24, 16, 8,
+      57, 49, 41, 33, 25, 17, 9, 1,
+      59, 51, 43, 35, 27, 19, 11, 3,
+      61, 53, 45, 37, 29, 21, 13, 5,
+      63, 55, 47, 39, 31, 23, 15, 7
+    ]
 
-# Tabel Permuted Choice 1 (PC-1)
-PC_1 = [
-    57, 49, 41, 33, 25, 17, 9,
-    1, 58, 50, 42, 34, 26, 18,
-    10, 2, 59, 51, 43, 35, 27,
-    19, 11, 3, 60, 52, 44, 36,
-    63, 55, 47, 39, 31, 23, 15,
-    7, 62, 54, 46, 38, 30, 22,
-    14, 6, 61, 53, 45, 37, 29,
-    21, 13, 5, 28, 20, 12, 4
-]
+# Expansion D-box Table
+exp_d = [
+      32, 1, 2, 3, 4, 5, 4, 5,
+      6, 7, 8, 9, 8, 9, 10, 11,
+      12, 13, 12, 13, 14, 15, 16, 17,
+      16, 17, 18, 19, 20, 21, 20, 21,
+      22, 23, 24, 25, 24, 25, 26, 27,
+      28, 29, 28, 29, 30, 31, 32, 1
+    ]
 
-# Tabel Permuted Choice 2 (PC-2)
-PC_2 = [
-    14, 17, 11, 24, 1, 5,
-    3, 28, 15, 6, 21, 10,
-    23, 19, 12, 4, 26, 8,
-    16, 7, 27, 20, 13, 2,
-    41, 52, 31, 37, 47, 55,
-    30, 40, 51, 45, 33, 48,
-    44, 49, 39, 56, 34, 53,
-    46, 42, 50, 36, 29, 32
-]
+# Straight Permutation Table
+per = [
+      16, 7, 20, 21,
+      29, 12, 28, 17,
+      1, 15, 23, 26,
+      5, 18, 31, 10,
+      2, 8, 24, 14,
+      32, 27, 3, 9,
+      19, 13, 30, 6,
+      22, 11, 4, 25
+    ]
 
-# Shift schedule for each round
-SHIFT_SCHEDULE = [
-    1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1
-]
-
-# Tabel Expansion (E)
-E = [
-    32, 1, 2, 3, 4, 5,
-    4, 5, 6, 7, 8, 9,
-    8, 9, 10, 11, 12, 13,
-    12, 13, 14, 15, 16, 17,
-    16, 17, 18, 19, 20, 21,
-    20, 21, 22, 23, 24, 25,
-    24, 25, 26, 27, 28, 29,
-    28, 29, 30, 31, 32, 1
-]
-
-# S-Boxes (Hanya S-box pertama sebagai contoh, Anda perlu mendefinisikan semua 8 S-box)
-S_BOX = {
-    1: [
-        [14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7],
-        [0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8],
-        [4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0],
-        [15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13],
+# S-box Table
+sbox = [
+    [
+        [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+      [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+      [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+      [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
     ],
-    # Definisikan S-box 2 hingga 8 di sini...
-    # ...
-}
-# Untuk kesederhanaan, kita hanya menggunakan S_BOX 1 dalam implementasi ini.
 
-# Tabel Permutation P
-P = [
-    16, 7, 20, 21,
-    29, 12, 28, 17,
-    1, 15, 23, 26,
-    5, 18, 31, 10,
-    2, 8, 24, 14,
-    32, 27, 3, 9,
-    19, 13, 30, 6,
-    22, 11, 4, 25
-]
+		[
+      [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+      [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+      [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+      [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]
+    ],
 
-def permute(block, table):
-    permuted_bits = [block[i - 1] for i in table]
-    return permuted_bits
+		[
+      [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+      [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+      [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+      [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]
+    ],
 
-def text_to_bits(plaintext):
-    result = []
-    for c in plaintext:
-        bits = format(ord(c), '08b')  # Mengonversi karakter ke 8-bit biner
-        result += [int(b) for b in bits]  # Menambahkan bit sebagai integer ke list hasil
-    return result
+		[
+      [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+      [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+      [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+      [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]
+    ],
 
-def bits_to_text(bits):
-    result = ''
-    for i in range(0, len(bits), 8):
-        byte = bits[i:i+8]  # Mengambil 8 bit
-        byte_str = ''.join(str(b) for b in byte)  # Mengonversi list bit menjadi string
-        result += chr(int(byte_str, 2))  # Mengonversi string bit ke karakter ASCII
-    return result
+		[
+      [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+      [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+      [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+      [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]
+    ],
 
-def pad_bits(bits, length):
-  if len(bits) < length:
-        return bits + [0] * (length - len(bits))
-  else:
-      return bits[:length]
+		[
+      [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+      [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+      [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+      [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]
+    ],
 
-def shift_left(key_half, n):
-    return key_half[n:] + key_half[:n]
+		[
+      [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+      [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+      [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+      [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]
+    ],
 
-def generate_subkeys(key):
-    key = permute(key, PC_1)
-    C = key[:28]
-    D = key[28:]
+		[
+      [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+      [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+      [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+      [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]
+    ]
+  ]
 
-    subkeys = []
-    for shift in SHIFT_SCHEDULE:
-        C = shift_left(C, shift)
-        D = shift_left(D, shift)
-        combine_cd = C + D
-        subkey = permute(combine_cd, PC_2)
-        subkeys.append(subkey)
+# Final Permutation Table
+final_perm = [
+      40, 8, 48, 16, 56, 24, 64, 32,
+			39, 7, 47, 15, 55, 23, 63, 31,
+			38, 6, 46, 14, 54, 22, 62, 30,
+			37, 5, 45, 13, 53, 21, 61, 29,
+			36, 4, 44, 12, 52, 20, 60, 28,
+			35, 3, 43, 11, 51, 19, 59, 27,
+			34, 2, 42, 10, 50, 18, 58, 26,
+			33, 1, 41, 9, 49, 17, 57, 25
+    ]
 
-    return subkeys
+# Hex to Bin and vice versa
+def hex2bin(s):
+	mp = {'0': "0000",
+		'1': "0001",
+		'2': "0010",
+		'3': "0011",
+		'4': "0100",
+		'5': "0101",
+		'6': "0110",
+		'7': "0111",
+		'8': "1000",
+		'9': "1001",
+		'A': "1010",
+		'B': "1011",
+		'C': "1100",
+		'D': "1101",
+		'E': "1110",
+		'F': "1111"}
+	bin = ""
+	for i in range(len(s)):
+		bin = bin + mp[s[i]]
+	return bin
 
-def feistel_function(right, subkey):
-    expanded_right = permute(right, E)
-    xor_output = [a ^ b for a, b in zip(expanded_right, subkey)]
-    sbox_output = []
-    for i in range(0, len(xor_output), 6):
-        block = xor_output[i:i+6]
-        row = int(str(block[0]) + str(block[5]), 2)
-        col = int(''.join(map(str, block[1:5])), 2)
-        sbox_val = S_BOX[1][row][col]
-        sbox_output.extend([int(bit) for bit in bin(sbox_val)[2:].zfill(4)])
-    
-    return permute(sbox_output, P)
+# Binary to hexadecimal conversion
 
-def encrypt(plaintext, key):
-    plaintext_bits = text_to_bits(plaintext)
-    key_bits = text_to_bits(key)
 
-    plaintext_bits = pad_bits(plaintext_bits, 64)
-    key_bits = pad_bits(key_bits, 64)
+def bin2hex(s):
+	mp = {"0000": '0',
+		"0001": '1',
+		"0010": '2',
+		"0011": '3',
+		"0100": '4',
+		"0101": '5',
+		"0110": '6',
+		"0111": '7',
+		"1000": '8',
+		"1001": '9',
+		"1010": 'A',
+		"1011": 'B',
+		"1100": 'C',
+		"1101": 'D',
+		"1110": 'E',
+		"1111": 'F'}
+	hex = ""
+	for i in range(0, len(s), 4):
+		ch = ""
+		ch = ch + s[i]
+		ch = ch + s[i + 1]
+		ch = ch + s[i + 2]
+		ch = ch + s[i + 3]
+		hex = hex + mp[ch]
 
-    initial_permutation = permute(plaintext_bits, IP)
-    left_half = initial_permutation[:32]
-    right_half = initial_permutation[32:]
+	return hex
 
-    subkeys = generate_subkeys(key_bits)
+# Binary to decimal conversion
 
-    for i in range(16):
-        temp_right = right_half
-        f_output = feistel_function(right_half, subkeys[i])
-        new_right = [a ^ b for a, b in zip(left_half, f_output)]
-        left_half = temp_right
-        right = new_right
+def bin2dec(binary):
 
-    combined_output = left_half + right_half
-    final_permutation = permute(combined_output, FP)
-    ciphertext = bits_to_text(final_permutation)
+	binary1 = binary
+	decimal, i, n = 0, 0, 0
+	while(binary != 0):
+		dec = binary % 10
+		decimal = decimal + dec * pow(2, i)
+		binary = binary//10
+		i += 1
+	return decimal
 
-    return ciphertext
+# Decimal to binary conversion
 
-def decrypt(ciphertext, key):
-    ciphertext_bits = text_to_bits(ciphertext)
-    key_bits = text_to_bits(key)
 
-    ciphertext_bits = pad_bits(ciphertext_bits, 64)
-    key_bits = pad_bits(key_bits, 64)
+def dec2bin(num):
+	res = bin(num).replace("0b", "")
+	if(len(res) % 4 != 0):
+		div = len(res) / 4
+		div = int(div)
+		counter = (4 * (div + 1)) - len(res)
+		for i in range(0, counter):
+			res = '0' + res
+	return res
 
-    initial_permutation = permute(ciphertext_bits, IP)
-    left_half = initial_permutation[:32]
-    right_half = initial_permutation[32:]
+# Permute function to rearrange the bits
 
-    subkeys = generate_subkeys(key_bits)
 
-    for i in range(15, -1, -1):
-        temp_left = left_half
-        f_output = feistel_function(left_half, subkeys[i])
-        new_left = [a ^ b for a, b in zip(left_half, f_output)]
-        left = new_left
-        right = temp_left
+def permute(k, arr, n):
+	permutation = ""
+	for i in range(0, n):
+		permutation = permutation + k[arr[i] - 1]
+	return permutation
 
-    combined_output = left_half + right_half
-    final_permutation = permute(combined_output, FP)
-    plaintext = bits_to_text(final_permutation)
+# shifting the bits towards left by nth shifts
 
-    return plaintext
 
-plaintext = input("Masukkan plaintext (maks 8 karakter): ")
-key = input("Masukkan key (maks 8 karakter): ")
+def shift_left(k, nth_shifts):
+	s = ""
+	for i in range(nth_shifts):
+		for j in range(1, len(k)):
+			s = s + k[j]
+		s = s + k[0]
+		k = s
+		s = ""
+	return k
 
-# Pastikan plaintext dan key tidak lebih dari 8 karakter
-if len(plaintext) > 8 or len(key) > 8:
-    print("Error: Plaintext dan key harus maksimal 8 karakter.")
+# calculating xow of two strings of binary number a and b
 
-ciphertext = encrypt(plaintext, key)
-print(f"Ciphertext (64-bit teks): {ciphertext}")
 
-decrypted_text = decrypt(ciphertext, key)
-print(f"Decrypted Text: {decrypted_text}")
+def xor(a, b):
+	ans = ""
+	for i in range(len(a)):
+		if a[i] == b[i]:
+			ans = ans + "0"
+		else:
+			ans = ans + "1"
+	return ans
+
+def encrypt(pt, rkb, rk):
+	pt = hex2bin(pt)
+
+	# Initial Permutation
+	pt = permute(pt, initial_perm, 64)
+
+	# Splitting
+	left = pt[0:32]
+	right = pt[32:64]
+	for i in range(0, 16):
+		# Expansion D-box: Expanding the 32 bits data into 48 bits
+		right_expanded = permute(right, exp_d, 48)
+
+		# XOR RoundKey[i] and right_expanded
+		xor_x = xor(right_expanded, rkb[i])
+
+		# S-boxex: substituting the value from s-box table by calculating row and column
+		sbox_str = ""
+		for j in range(0, 8):
+			row = bin2dec(int(xor_x[j * 6] + xor_x[j * 6 + 5]))
+			col = bin2dec(
+				int(xor_x[j * 6 + 1] + xor_x[j * 6 + 2] + xor_x[j * 6 + 3] + xor_x[j * 6 + 4]))
+			val = sbox[j][row][col]
+			sbox_str = sbox_str + dec2bin(val)
+
+		# Straight D-box: After substituting rearranging the bits
+		sbox_str = permute(sbox_str, per, 32)
+
+		# XOR left and sbox_str
+		result = xor(left, sbox_str)
+		left = result
+
+		# Swapper
+		if(i != 15):
+			left, right = right, left
+
+	# Combination
+	combine = left + right
+
+	# Final permutation: final rearranging of bits to get cipher text
+	cipher_text = permute(combine, final_perm, 64)
+	return cipher_text
+
+def hex2ascii(hexstr):
+ 
+    # initialize the ASCII code string as empty.
+    ascii = ""
+ 
+    for i in range(0, len(hexstr), 2):
+ 
+        # extract two characters from hex string
+        part = hexstr[i : i + 2]
+ 
+        # change it into base 16 and
+        # typecast as the character 
+        ch = chr(int(part, 16))
+ 
+        # add this char to final ASCII string
+        ascii += ch
+
+    return ascii
+
+pt = "4B6170616C617764"
+key = "AABB09182736CCDD"
+
+# Key generation
+# --hex to binary
+key = hex2bin(key)
+
+# --parity bit drop table
+keyp = [57, 49, 41, 33, 25, 17, 9,
+		1, 58, 50, 42, 34, 26, 18,
+		10, 2, 59, 51, 43, 35, 27,
+		19, 11, 3, 60, 52, 44, 36,
+		63, 55, 47, 39, 31, 23, 15,
+		7, 62, 54, 46, 38, 30, 22,
+		14, 6, 61, 53, 45, 37, 29,
+		21, 13, 5, 28, 20, 12, 4]
+
+# getting 56 bit key from 64 bit using the parity bits
+key = permute(key, keyp, 56)
+
+# Number of bit shifts
+shift_table = [1, 1, 2, 2,
+			2, 2, 2, 2,
+			1, 2, 2, 2,
+			2, 2, 2, 1]
+
+# Key- Compression Table : Compression of key from 56 bits to 48 bits
+key_comp = [14, 17, 11, 24, 1, 5,
+			3, 28, 15, 6, 21, 10,
+			23, 19, 12, 4, 26, 8,
+			16, 7, 27, 20, 13, 2,
+			41, 52, 31, 37, 47, 55,
+			30, 40, 51, 45, 33, 48,
+			44, 49, 39, 56, 34, 53,
+			46, 42, 50, 36, 29, 32]
+
+# Splitting
+left = key[0:28] # rkb for RoundKeys in binary
+right = key[28:56] # rk for RoundKeys in hexadecimal
+
+rkb = []
+rk = []
+for i in range(0, 16):
+	# Shifting the bits by nth shifts by checking from shift table
+	left = shift_left(left, shift_table[i])
+	right = shift_left(right, shift_table[i])
+
+	# Combination of left and right string
+	combine_str = left + right
+
+	# Compression of key from 56 to 48 bits
+	round_key = permute(combine_str, key_comp, 48)
+
+	rkb.append(round_key)
+	rk.append(bin2hex(round_key))
+
+# Main
+
+strinp = input('Enter a 8-length string: ')
+
+if len(strinp) == 8:
+  hexstring = ''
+  for i in strinp:
+    # print(hex(ord(i))[2:])
+    hexstring += hex(ord(i))[2:]
+
+  hexstring = hexstring.upper()
+
+  print('Hex String: ', hexstring)
+
+  print("Encryption")
+  cipher_text = bin2hex(encrypt(hexstring, rkb, rk))
+  print("Cipher Text : ", hex2ascii(cipher_text))
+
+  print("Decryption")
+  rkb_rev = rkb[::-1]
+  rk_rev = rk[::-1]
+  hextext = bin2hex(encrypt(cipher_text, rkb_rev, rk_rev))
+  final_ascii = hex2ascii(hextext)
+  print("Plain Text : ", final_ascii)
+
+else:
+  print('String must consist of 8 chars')
+  exit()
